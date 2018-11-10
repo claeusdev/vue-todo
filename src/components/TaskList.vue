f6e3ff7400631c6d1e618b71365971560b82eef2
Nana Adjei Manu <claeusdev@gmail.com>
2018-11-10 17:01:36 -0500

    Feat: implement add and remove tasks

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-      Tasks Go here
+    <input type="text" class="task-input" 
+    placeholder="What do you need to do?" v-model="newTask" @keyup.enter="addTask">
+    <div v-for="(task, index) in tasks" :key="task.id" class="task">
+      <div class="task__title">
+        {{ task.title }}
+      </div>
+
+      <div class="task__actions" >
+        <div class="remove-task" @click="removeTask(index)">
+          &times;
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -9,13 +22,74 @@ export default {
   name: 'task-list',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      newTask: '',
+      idForTask: 4,
+      tasks: [
+        {
+          id: 1,
+          title: 'Finish learning Vue',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Learn React Native',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Learn Swift',
+          completed: false,
+        },
+      ],
     };
+  },
+  methods: {
+    addTask() {
+
+      if (this.newTask.trim().length === 0) {
+        return
+      }
+      this.tasks.push({
+        id: this.idForTask,
+        title: this.newTask,
+        completed: false,
+      })
+
+      this.newTask = '';
+      this.idForTask++;
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1)
+    }
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style >
+<style lang='scss'>
+  .task-input {
+    width: 100%;
+    padding: 10px;
+    font-size: 18px;
+    margin-bottom: 15px;
+    &:focus {
+      outline: 0;
+    }
+  }
 
+  .task {
+      margin-bottom: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .remove-task {
+      cursor: pointer;
+      margin-left: 14px;
+
+      &:hover {
+        color: red;
+      }
+    }
 </style>
