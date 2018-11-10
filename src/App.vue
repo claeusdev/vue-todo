c7e5f0406e690d24d0d7829b1cee153adacb8529
Nana Adjei Manu <claeusdev@gmail.com>
2018-11-10 16:19:52 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="wrapper">
+    <img src="./assets/logo.png">
+    <task-list></task-list>
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList';
+
+export default {
+  name: 'App',
+  components: {
+    TaskList,
+  },
+};
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+.wrapper {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
